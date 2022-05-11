4886e5ae2cb5eca07a547cd44f2b7671f6020a88
evprado849 <103149284+evprado849@users.noreply.github.com>
2022-05-10 19:33:18 -0700

    Create CSE15LDemoRegrade.java

---
+++ CSE15LDemoRegrade.java
@@ -0,0 +1,9 @@
+public class CSE15LDemoRegrade {
+  public static int multiplyTwoInts(int first, int second) {...}
+  
+  public static void main(String[] args) {
+    System.out.println("This is my CSE15L Skill Demo Regrade Video.");
+  }
+
+
+}
